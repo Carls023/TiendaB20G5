34f44cd4e4a163c26e6d65164b7ca4ef9e3ca554
edwin1302 <90660248+edwin1302@users.noreply.github.com>
2021-12-05 19:41:15 -0500

    change port

--- models/server.js
+++ models/server.js
@@ -7,8 +7,8 @@ class Server {
     
     constructor(){
         this.app = express();
-        //this.port = process.env.PORT | 3000;
-        this.port = 3000;
+        this.port = process.env.PORT | 5000;
+        //this.port = 3000;
         this.productsPath = '/api/products';
         this.usersPath = '/api/users';
 
